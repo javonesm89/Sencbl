2e13f164f6c2a222f3f06bf73f4580235711b6a1
javonesm89 <javosmith12@gmail.com>
2020-08-26 12:25:25 -0400

    Update cli.rb

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,4 +4,17 @@ class CLI
     attr_accessor  :word_hash, :has_name
     
     @@prompt = TTY::Prompt.new
+
+    def run
+        if !@has_name 
+             greet
+             @has_name = true
+        end
+        Scraper.collect_dates
+        @date_options = Scraper.all
+        @chosen_date = @@prompt.select("#{@name}, choose a date for Word of the Day!", @date_options)
+        scrape_and_make_word_object(@chosen_date)
+        display
+        start_over
+    end
 end
