6140374abd9c38449f673bde5b6b8b740a9c0c4f
javonesm89 <javosmith12@gmail.com>
2020-08-26 12:22:35 -0400

    Update word.rb

--- lib/word.rb
+++ lib/word.rb
@@ -1,4 +1,7 @@
 require "../Sencbl/environment.rb"
 
 class Word
+    attr_reader :word, :word_class, :define, :origin_prefix, :origin, :examples_prefix, :examples, :chosen_date, :pronunciation
+
+    @@words = []
 end
